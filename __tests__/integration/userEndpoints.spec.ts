8739dd44fec45427b11bcd983c18667ebb7b165d
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2022-04-01 16:14:11 -0300

    feat: github actions workflow
    feat: jest config
    
    feat: eslint config
    
    feat: serverless.yml
    
    feat: dependencies
    
    feat: express app
    
    feat: default route
    
    refactor: using serverless-http module
    
    feat: ajust test dev stage
    
    fix: github actions yml and serverless yml
    
    feat: base repository
    
    feat: user model
    
    feat: user repository
    
    feat: user creation service
    
    feat: get user service
    
    feat: error handling middleware
    
    feat: user endpoint
    
    fix: dynamo repository constructor syntax
    
    fix: configs
    
    feat: local environment
    
    feat: dynamodb indexes in serverless.yml
    
    feat: ajust user service
    
    fix: serverless package exclude node_modules
    
    fix: lambda package exclude heavy folders/files
    
    feat: serverless reduce plugin
    
    fix: serverless ignore missing modules
    
    fix: missing modules
    
    fix: removed more useless modules
    
    fix: added jest to prod dependencies
    
    feat: ajustments
    
    feat: added serverless-iam-roles-per-function plugin
    
    fix: commented local plugins
    
    fix: prevent dynamoose from creating table
    
    fix: abstract iam dynamodb permissions
    
    feat: table query and scan index permission
    
    feat: access counter service
    
    feat: access counter endpoints
    
    fix: user service execution order
    
    fix: typo
    
    feat: reestablished local db
    
    feat: main branch pipeline
    
    feat: user password checking
    
    feat: README documentation
    
    fix: users return
    
    feat: link swagger no README
    
    fix: pipeline yml deploy stage

---
+++ __tests__/integration/userEndpoints.spec.ts
@@ -0,0 +1,120 @@
+import app from '../../src/app'
+import supertest from 'supertest'
+
+import userRepository from '../../src/modules/users/user.repository'
+import { User } from '../../src/modules/users/user.model'
+import { PinpointEmail } from 'aws-sdk'
+
+jest.mock('dynamoose')
+jest.mock('aws-sdk')
+jest.mock('../../src/modules/users/user.repository')
+
+const mockedRepository = userRepository as jest.Mocked<typeof userRepository>
+
+let mockUser
+let mockPartialUser
+
+beforeEach(() => {
+  jest.clearAllMocks()
+
+  mockUser = {
+    username: 'user',
+    password: 'Senha123',
+    email: 'user@email.com.br'
+  }
+
+  mockPartialUser = {
+    username: 'user',
+    password: 'Senha123',
+  }
+})
+
+describe('user endpoints', () => {
+  it('/POST should respond 400 if required fields in the body are missing', async () => {
+    mockedRepository.insert.mockImplementation((user: Partial<User>) => Promise.resolve(user as User))
+    mockedRepository.findOne.mockResolvedValue(null)
+
+    const res = await supertest(app)
+      .post('/users')
+      .send(mockPartialUser)
+
+    expect(res.status).toBe(400)
+  })
+  
+  it('/POST should respond 400 if email is already used', async () => {
+    mockedRepository.insert.mockImplementation((user: Partial<User>) => Promise.resolve(user as User))
+    mockedRepository.findOne.mockResolvedValueOnce({email: mockUser.email} as User)
+    mockedRepository.findOne.mockResolvedValueOnce(null as User)
+
+    const res = await supertest(app)
+      .post('/users')
+      .send(mockUser)
+
+    expect(res.status).toBe(400)
+  })
+  
+  it('/POST should respond 400 if username is already used', async () => {
+    mockedRepository.insert.mockImplementation((user: Partial<User>) => Promise.resolve(user as User))
+    mockedRepository.findOne.mockResolvedValueOnce({username: mockUser.username} as User)
+    mockedRepository.findOne.mockResolvedValueOnce(null as User)
+
+    const res = await supertest(app)
+      .post('/users')
+      .send(mockUser)
+    
+    expect(res.status).toBe(400)
+  })
+ 
+  it('/POST should only accept passwords that are ate least 8 chars long, at least one uppercase and one number', async () => {
+    mockedRepository.insert.mockImplementation((user: Partial<User>) => Promise.resolve(user as User))
+    mockedRepository.findOne.mockResolvedValue(null)
+
+    mockUser = {...mockUser, password: '1234'}
+
+    const res = await supertest(app)
+      .post('/users')
+      .send(mockUser)
+    
+    expect(res.status).toBe(400)
+  })
+
+  it('/POST should respond 201 if the right body data is passed', async () => {
+    mockedRepository.insert.mockImplementation((user: Partial<User>) => Promise.resolve(user as User))
+    mockedRepository.findOne.mockResolvedValue(null)
+
+    const res = await supertest(app)
+      .post('/users')
+      .send(mockUser)
+
+    expect(res.status).toBe(201)
+  })
+  
+  it('/POST should respond 201 if the right body data is passed', async () => {
+    mockedRepository.insert.mockImplementation((user: Partial<User>) => Promise.resolve(user as User))
+    mockedRepository.findOne.mockResolvedValue(null)
+
+    const res = await supertest(app)
+      .post('/users')
+      .send(mockUser)
+
+    expect(res.status).toBe(201)
+  })
+
+  it('/GET should respond 404 if resource doesnt exists', async () => {
+    mockedRepository.findOne.mockResolvedValue('' as unknown as User)
+
+    const res = await supertest(app)
+      .get('/users/1')
+    
+    expect(res.status).toBe(404)
+  })
+
+  it('/GET should respond 200 if resource exists', async () => {
+    mockedRepository.findOne.mockResolvedValue(mockUser)
+
+    const res = await supertest(app)
+      .get('/users/1')
+    
+    expect(res.status).toBe(200)
+  })
+})
---
+++ src/modules/users/user.service.ts
@@ -0,0 +1,55 @@
+import { Repository } from '../../base/repositories/interfaces/IRepository'
+import { User } from './user.model'
+import userRepository from './user.repository'
+import { CreateUserDto} from './dtos/createUser.dto'
+import bcrypt from 'bcrypt'
+import { Response } from 'express'
+
+export interface IUserService {
+  create(user: CreateUserDto): Promise<User>
+  get(id: string): Promise<User>
+	getAll(): Promise<User[]>
+}
+
+export class UserService implements IUserService {
+
+	constructor(private readonly repository: Repository<User>){}
+
+	async create(user: CreateUserDto): Promise<User> {
+		const emailAlreadyUsed = await this.repository.findOne({email: user.email})
+		
+		if(emailAlreadyUsed) throw new Error('Email already registered')
+		
+		const usernameAlreadyUsed = await this.repository.findOne({username: user.username})
+		
+		if(usernameAlreadyUsed) throw new Error('Username already registered')
+		
+		const passwordMatch = user.password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/)
+
+		if(!passwordMatch) throw new Error('Senha deve ter ao menos 8 caracteres, sendo pelo menos um miúsculo e uma letra')
+
+		const encryptedPassword = await bcrypt.hash(user.password, 8)
+
+		delete user.password
+
+		return await this.repository.insert({
+			email: user.email,
+			passwordHash: encryptedPassword,
+			username: user.username
+		})
+	}
+
+	async get(id: string): Promise<User> {
+		const res = await this.repository.findOne(id)  
+
+		if(res) delete res.passwordHash
+
+		return res
+	}
+
+	async getAll(): Promise<User[]>{
+		return await this.repository.find()
+	}
+}
+
+export default new UserService(userRepository)
