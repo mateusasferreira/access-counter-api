bb6e5352759724da9f54f164d319fc2001eebb8f
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2022-04-01 17:36:21 -0300

    feat: default route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,8 @@
 import {Request, Response, Router} from 'express'
+import defaultRoute from './modules/default/default.route'
 
 const routes = Router()
 
-routes.get('/', (req: Request, res: Response) => {
-	res.sendStatus(200)
-})
+routes.use('/', defaultRoute)
 
 export default routes 
